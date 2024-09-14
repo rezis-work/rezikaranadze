710255c02e762aea8ef06fcd0733e239ad13bf88
Rezi Karanadze <karanadzerevazi@gmail.com>
2024-09-15 00:22:08 +0400

    setup portfolio project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <main className=" max-w-7xl mx-auto">
+      <h1 className=" text-2xl text-white underline">Hello, Three js</h1>
+    </main>
+  );
+};
+
+export default App;
